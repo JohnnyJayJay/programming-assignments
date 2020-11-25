fd70f4c3cc15452602b673f2dc3407b8121ae44f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-11-25 16:03:00 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    checkstyle
+}
+
+group = "edu.kit.programming"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.0")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.7.0")
+}
+
+checkstyle {
+    config = resources.text.fromFile("config/checkstyle/optionalCheckstyleRules.xml")
+}
+
+val packageDeclaration = "^\\s*package\\s+[^;]+;$".toRegex()
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    register<Copy>("finalize") {
+        dependsOn("checkstyleMain", "test")
+        from("src/main/java")
+        into("build/final")
+        filter {
+            it.replace(packageDeclaration, "")
+        }
+    }
+}
